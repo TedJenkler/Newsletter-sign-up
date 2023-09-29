d2a1f08b6d3c2860598c2c92d410b9115f7908ec
TedJenkler <Tedjenkler@gmail.com>
2023-09-29 05:07:27 -0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import mainImg from './images/illustration-sign-up-mobile.svg'
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='row'>
+        <img src={mainImg}></img>
+      </div>
+      <div className='row'>
+        <h1>Stay updated!</h1>
+        <p>Join 60,000+ product managers receiving monthly updates on:</p>
+        <ol>
+          <li>Product discovery and building what matters</li>
+          <li>Measuring to ensure updates are a success</li>
+          <li>And much more!</li>
+        </ol>
+      <div className='row'>
+        <form className='subscribe-form'>
+          <label className='emaillabel'>Email address</label>
+          <input className='emailinput' type='text' placeholder='email@company.com'></input>
+          <button className='submit' type='submit'>Subscribe to monthly newsletter</button>
+        </form>
+      </div>
+      </div>
     </div>
   );
 }
